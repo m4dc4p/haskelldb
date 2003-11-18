3d83a9724304af34d61d1182765d8ab616877dcf
forester <haskelldb-users@lists.sourceforge.net>
2003-11-18 02:38:15 -0800

    *** empty log message ***
    darcs-hash:20031118103815-3bf50-aaf69141bdc9fef9f4ae67ef38e1d0ae91027531.gz

--- src/Database/HaskellDB/HDBRec.hs
+++ src/Database/HaskellDB/HDBRec.hs
@@ -1,22 +1,39 @@
+-- | This is a simple replacement for TREX.
 module HDBRec where
 
+-- * Data declarations.
+
+-- | Last entry in each record.
 data HDBRecTail = HDBRecTail
+
+-- | Constuctor placed between every entry in a record.
+-- a is an entry and b is the remaining record.
 data HDBRecSep a b = HDBRecSep a b
+
+-- | Header of every record.
 data HDBRec r = HDBRec r
 
+-- * Class definitions.
+
+-- | Each entry in a record needs to be an instance of this class.
+-- Fundeps and two argument classes are used, this violates haskell 98 standard. 
 class HDBRecEntry a b | a -> b where
     fieldName :: a b -> String
     fieldValue :: a b -> b
 
+-- | A record must belong to this class to be showable.
 class ShowRecRow r where
     showRecRow :: r -> [(String,ShowS)]
 
+-- Last entry in each record will terminate the ShowrecRow recursion.
 instance ShowRecRow HDBRecTail where
     showRecRow _ = []
 
+-- Recurse a record and produce a showable tuple.
 instance (HDBRecEntry a b, Show b, ShowRecRow c) => ShowRecRow (HDBRecSep (a b) c) where
     showRecRow (HDBRecSep f fs) = 
 	(fieldName f, const (show (fieldValue f))) : showRecRow fs
+
 
 instance ShowRecRow r => ShowRecRow (HDBRec r) where
     showRecRow (HDBRec r) = showRecRow r
--- src/HDBRecUtils.hs
+++ src/HDBRecUtils.hs
@@ -1,31 +1,37 @@
+-- | Utility functions for HDBRec.
 module HDBRecUtils where
 
 import HDBRec
 import Query
 import PrimQuery
 
---
--- Utility functions
---
+-- * Functions
 
+-- Internal function used primarily in hdbProject and hdbBaseTable.
 hdbMakeRec :: (HDBRecTail -> r) -> HDBRec r
 hdbMakeRec f = HDBRec (f HDBRecTail)
 
+-- | Constructs an entry with type and name of field.
 hdbMakeEntry :: (Expr a -> b) -> String -> c -> HDBRecSep b c
 hdbMakeEntry f n = HDBRecSep (f (attribute n)) 
 
+-- | Project a HDBRec on the database.
 hdbProject :: (ShowRecRow r) => (HDBRecTail -> r) -> Query (Rel r)
 hdbProject = Query.project . hdbMakeRec
 
+-- | Constructs a virtual table.
 hdbBaseTable :: ShowRecRow r => String -> (HDBRecTail -> r) -> Table r
 hdbBaseTable name = Query.baseTable name . hdbMakeRec
 
+-- * Operators
 
 infix  6 .<<.
 infixr 5 #
 
+-- | Links together a type and a value into an entry.
 ( .<<. ) :: (a -> b) -> a -> c -> HDBRecSep b c
 f1 .<<. f2 = HDBRecSep $ f1 f2
 
+-- | Links two records together.
 ( # ) :: (b -> c) -> (a -> b) -> a -> c
 f1 # f2 = f1 . f2
