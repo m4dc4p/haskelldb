362326525c373bf0893a3ae070cb81fe00b7e599
d00bring <haskelldb-users@lists.sourceforge.net>
2004-01-05 15:11:30 -0800

    moved HSQL build out of source tree
    darcs-hash:20040105231130-cca28-30950e3737ba0631eb32ac6a11f501914638306a.gz

--- src/config.mk
+++ src/config.mk
@@ -2,7 +2,17 @@ GHC = ghc
 GHCI = ghci
 GHCFLAGS = -fglasgow-exts -fallow-overlapping-instances
 
-
 HUGS=hugs
 RUNHUGS=runhugs
 HUGSFLAGS=-98 +o
+
+HSQL_DIR = ../../../../HSQL-cvs
+UNIXODBC_PREFIX = /usr
+
+HOST = $(shell hostname)
+ifeq ("$(HOST)", "jackass.tekno.chalmers.se")
+HSQL_DIR = /usr/local/dp03-7/HSQL-cvs
+UNIXODBC_PREFIX=/usr/local/dp03-7/unixODBC-2.2.6
+endif
+
+UNIXODBC_LIBDIR = $(UNIXODBC_PREFIX)/lib
