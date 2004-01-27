2c63ada06ac9e16a5ca9140e7c6428724d87da7b
chucky <haskelldb-users@lists.sourceforge.net>
2004-01-27 10:17:43 -0800

    Added preliminary types for Date, Time, DateTime and Timestamp. They are
    Added preliminary types for Date, Time, DateTime and Timestamp. They are
    not usable yet and the interface is not done, but the code doesn't break
    anything existing
    
    darcs-hash:20040127181743-29341-f8e72edda71a33b53e023fb0a05cf0fe1faa631d.gz

--- src/Database/HaskellDB/FieldType.hs
+++ src/Database/HaskellDB/FieldType.hs
@@ -1,11 +1,40 @@
 module FieldType (FieldDef, FieldType(..)) where
 
+import System.Time
+
 {-
  TODO:
 
  - add date / time type(s)
 
 -}
+
+newtype Date = D (Int,Int,Int)
+    deriving (Eq,Ord)
+newtype Time = T (Int,Int,Int)
+    deriving (Eq,Ord)
+-- interestingly enough deriving Ord automatically for DateTime seems to work
+-- I have tested it and it seems so anyway. / Chucky
+newtype DateTime = DT (Date,Time)
+    deriving (Eq,Ord)
+type Timestamp = DateTime
+
+-- we need to provide better definitions of show using showsPrec, and also
+-- of readsPrec (to give us read)
+instance Show Date where
+    show (D (a,b,c)) = show a ++ "-" ++ show b ++ "-" ++ show c
+{-instance Read Date where
+    read s = (year,month,day)
+	where
+	year = read (take 4 xs)
+	month = read (take 2 (drop 5 xs))
+	day = read (take 2 (drop 8 xs))
+-}
+instance Show Time where
+    show (T (a,b,c)) = show a ++ ":" ++ show b ++ ":" ++ show c
+
+instance Show DateTime where
+    show (DT (d,t)) = show d ++ " " ++ show t
 
 -- | The type and @nullable@ flag of a database column
 type FieldDef = (FieldType, Bool)
