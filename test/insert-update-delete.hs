b91bdde2f1684b3ad71a49d4b9d99332eb8dfd5b
d00bring <haskelldb-users@lists.sourceforge.net>
2004-02-02 11:03:55 -0800

    added insert, delete, update test
    darcs-hash:20040202190355-cca28-52cf61f6a135fd6e2ed5fca2140d49dac40e8c1b.gz

--- test/insert-update-delete.hs
+++ test/insert-update-delete.hs
@@ -1,14 +1,19 @@
 import Database.HaskellDB
 import Dp037.Test_tb1
 
-import TestConnect
+--import Database.HaskellDB.HSQL.PostgreSQL
+import Database.HaskellDB.HSQL.ODBC
+
+opts = ODBCOptions { dsn = "mysql-dp037", uid = "dp037", pwd = "teent333" }
+withDB = odbcConnect opts
+
 
 printTable db =
     do
     t <- query db (table test_tb1)
     mapM_ (putStrLn . showRow) t
 
-showRow r = lpad 10 (show (r!c11)) ++ lpad 10 (showNullable (r!c12))
+showRow r = lpad 10 (show (r!.c11)) ++ lpad 10 (showNullable (r!.c12))
 
 showNullable :: Show a => Maybe a -> String
 showNullable = maybe "NULL" show
@@ -22,14 +27,9 @@ testInsert db =
     insert db test_tb1 (c11 << constant 157 # c12 << constant (Just 56))
     insert db test_tb1 (c11 << constant (-567) # c12 << constant Nothing)
 
-testUpdate db = 
-    do
-    update db test_tb1 (\r -> r!c11 .==. constant 157) (setC12 (Just 18))
-    where
-    setC12 x r = c12 << constant x
-testDelete db = 
-    do
-    delete db test_tb1 (\r -> r!c11 .==. constant 157 .||. isNull (r!c12))
+testUpdate db = return ()
+testDelete db = return ()
+
 
 test db = 
     do
@@ -45,4 +45,4 @@ test db =
     putStrLn "After delete:"
     printTable db
 
-main = argConnect test
+main = withDB test
