1182d3fa749bf8a4e5016803048da220fdeee48f
d00bring <haskelldb-users@lists.sourceforge.net>
2004-02-22 03:07:02 -0800

    added simple transaction testing program.
    darcs-hash:20040222110702-cca28-7156aee92f5eac19e0fd22d4ac69bad9b229acbb.gz

--- test/transaction.hs
+++ test/transaction.hs
@@ -1,29 +1,30 @@
 import Database.HaskellDB
-
-import TestConnect
-
+import Database.HaskellDB.HSQL.MySQL
 import Control.Exception
 import Dp037.Test_tb1
 
-printRecord r = putStrLn $ show (r!c11) ++ " " ++ show (r!c12)
+--opts = ODBCOptions{dsn="", uid="", pwd=""}
+--withDB f = odbcConnect opts f
+
+opts = MySQLOptions{server="localhost", db="", uid="", pwd=""}
+withDB f = mysqlConnect opts f
+
+printRecord r = putStrLn $ show (r!.c11) ++ " " ++ show (r!.c12)
 
 printRecords db = do
 		  rs <- query db (table test_tb1)
-		  mapM_ printRecord rs
+		  mapM printRecord rs
 
 testTrans db = do
-	       insert db test_tb1 (c11 << constant 2 # c12 << constJust (-67))
+	       insert db test_tb1 (c11 << constant 2 # c12 << nullable (-67))
 	       fail "oops, that didn't work out"
-
-handleErr io = catchJust userErrors io 
-	        (\e -> putStrLn $ "caught nasty exception: " ++ e)
 
 test db = do
 	  putStrLn "before transaction:"
-	  handleErr $ printRecords db
-	  handleErr $ transaction db (testTrans db)
+	  printRecords db
+	  catchJust userErrors (transaction db (testTrans db)) (\e -> putStrLn $ "exception: " ++ e)
 	  putStrLn "after transaction:"
-	  handleErr $ printRecords db
+	  printRecords db
 
 
-main = argConnect test
+main = withDB test
