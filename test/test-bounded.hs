53f0564ff5dede32a64d55aeed2bed52f2c443b9
d00bring <haskelldb-users@lists.sourceforge.net>
2004-02-26 23:29:12 -0800

    added a simple test that uses BoundedString
    darcs-hash:20040227072912-cca28-42176baa61606933be038d4051ab091ab0d4d90b.gz

--- test/test-bounded.hs
+++ test/test-bounded.hs
@@ -1,20 +1,21 @@
 import System.Environment
 
-import TestConnect
-
 import Database.HaskellDB
 import Database.HaskellDB.BoundedList
 import Database.HaskellDB.HSQL.ODBC
 import Dp037_bounded.D3proj_users
+
+opts = ODBCOptions{dsn="mysql-dp037", uid="dp037", pwd="teent333"}
+withDB f = odbcConnect opts f
 
 q n = do
       users <- table d3proj_users
       restrict (users!xid .==. constant (trunc n))
       project (first_name << users!first_name # last_name << users!last_name)
 
-printName r = putStrLn (fromBounded (r!first_name) ++ " " 
-			++ fromBounded (r!last_name))	      
+printName r = putStrLn (fromBounded (r!.first_name) ++ " " 
+			++ fromBounded (r!.last_name))	      
 
 main = do
-       connArgs <- getArgs
-       connect connArgs (\db -> query db (q "d00bring") >>= mapM_ printName) 
+       [user] <- getArgs
+       withDB (\db -> query db (q user) >>= mapM_ printName) 
