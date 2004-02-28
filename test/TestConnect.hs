1502449bd97be75603e35180cf6cce91c30bd737
d00bring <haskelldb-users@lists.sourceforge.net>
2004-02-28 11:38:37 -0800

    added TestConnect.hs
    darcs-hash:20040228193837-cca28-66b091d6b77f63f2e2545de937ed9f2dcfbaf1e0.gz

--- test/TestConnect.hs
+++ test/TestConnect.hs
@@ -1,14 +1,43 @@
+{-# LINE 1 "TestConnect.pphs" #-}
 module TestConnect where
 
-import Database.HaskellDB
-import Database.HaskellDB.GenericConnect
 import System.Environment
 
-connect :: [String] -> (Database -> IO a) -> IO a
-connect (driver:args) = genericConnect driver args
-connect _ = error "No driver argument supplied"
+-- #define 1
+-- #define 1
+-- #define 1
 
-argConnect :: (Database -> IO a) -> IO a
+import Database.HaskellDB.HSQL.Common
+
+import qualified Database.HaskellDB.HSQL.ODBC as OD
+
+
+import qualified Database.HaskellDB.HSQL.MySQL as MY
+
+
+import qualified Database.HaskellDB.HSQL.PostgreSQL as PG
+
+
+connect :: [String] -> (HSQL -> IO a) -> IO a
+
+connect ["ODBC",d,u,p] = OD.odbcConnect OD.ODBCOptions 
+				     {OD.dsn = d,OD.uid = u,OD.pwd = p}
+
+
+connect ["MySQL",s,d,u,p] = MY.mysqlConnect MY.MySQLOptions 
+			               {MY.server = s,MY.db = d, 
+					MY.uid = u,MY.pwd = p}
+
+
+connect ["PostgreSQL",s,d,u,p] = PG.postgresqlConnect PG.PostgreSQLOptions 
+			               {PG.server = s,PG.db = d, 
+					PG.uid = u,PG.pwd = p}
+
+connect _ = error $ unlines ["Accepted options:",
+			     "ODBC dsn uid pwd",
+			     "MySQL server db uid pwd",
+			     "PostgreSQL server db uid pwd"]
+
 argConnect f = do
-               args <- getArgs
-               connect args f
+	       args <- getArgs 
+	       connect args f
