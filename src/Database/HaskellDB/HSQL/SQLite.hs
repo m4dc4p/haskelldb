7f70eea2e75af274470d4afc84b9fa24b503687d
d00bring <haskelldb-users@lists.sourceforge.net>
2004-03-07 17:20:51 -0800

    Added the SQLite module, which should have been in the commit of stepcut's patch yesterday, but which I forgot to add.
    darcs-hash:20040308012051-cca28-51d36041a4f2ed94157d9a8cbe753e501fbcf8c9.gz

--- src/Database/HaskellDB/HSQL/SQLite.hs
+++ src/Database/HaskellDB/HSQL/SQLite.hs
@@ -2,27 +2,24 @@
 -- |
 -- Module      :  HaskellDB
 -- Copyright   :  Daan Leijen (c) 1999, daan@cs.uu.nl
---                HWT Group (c) 2003, haskelldb-users@lists.sourceforge.net
+--                HWT Group (c) 2003, dp03-7@mdstud.chalmers.se
 -- License     :  BSD-style
 -- 
--- Maintainer  :  haskelldb-users@lists.sourceforge.net
+-- Maintainer  :  dp03-7@mdstud.chalmers.se
 -- Stability   :  experimental
--- Portability :  non-portable
+-- Portability :  portable
 --
 -- Interface to the SQLite <http://www.hwaci.com/sw/sqlite/>
 -- databases.
---
--- $Revision: 1.6 $
+-- 
 -----------------------------------------------------------
 module Database.HaskellDB.HSQL.SQLite (
-		   SQLiteOptions(..),
-		   sqliteConnect,
-		   driver
+		   SQLiteOptions(..)
+		  , sqliteConnect
 		  ) where
 
 import Database.HaskellDB.Database
 import Database.HaskellDB.HSQL.Common
-import Database.HaskellDB.DriverAPI
 import qualified Database.HSQL.SQLite as SQLite (connect) 
 import System.IO
 
@@ -35,24 +32,3 @@ sqliteConnect :: SQLiteOptions -> (Database -> IO a) -> IO a
 sqliteConnect = 
     hsqlConnect (\opts -> SQLite.connect 
 		            (filepath opts) (mode opts))
-
-sqliteConnectOpts :: [(String,String)] -> (Database -> IO a) -> IO a
-sqliteConnectOpts opts f = 
-    do
-    [a,b] <- getOptions ["filepath","mode"] opts
-    m <- readIOMode b
-    sqliteConnect (SQLiteOptions {filepath = a,
-				  mode = m}) f
-
-readIOMode :: Monad m => String -> m IOMode
-readIOMode s = 
-    case s of
-           "r" -> return ReadMode
-           "w" -> return WriteMode
-           "a" -> return AppendMode
-           "rw" -> return ReadWriteMode
-           _ -> case reads s of
-                             [(x,"")] -> return x
-                             _ -> fail $ "Bad IO mode: " ++ s
-
-driver = defaultdriver {connect = sqliteConnectOpts}
