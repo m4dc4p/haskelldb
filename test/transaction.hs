ab65925da3b31a36abc89e8eaf7bacc04507a16e
d00bring <haskelldb-users@lists.sourceforge.net>
2004-03-22 13:38:10 -0800

    catch errors from all db functions
    darcs-hash:20040322213810-cca28-22e59f8e774e33ed2a302403dff4d23ab5a4912c.gz

--- test/transaction.hs
+++ test/transaction.hs
@@ -9,18 +9,21 @@ printRecord r = putStrLn $ show (r!.c11) ++ " " ++ show (r!.c12)
 
 printRecords db = do
 		  rs <- query db (table test_tb1)
-		  mapM printRecord rs
+		  mapM_ printRecord rs
 
 testTrans db = do
 	       insert db test_tb1 (c11 << constant 2 # c12 << constJust (-67))
 	       fail "oops, that didn't work out"
 
+handleErr io = catchJust userErrors io 
+	        (\e -> putStrLn $ "caught nasty exception: " ++ e)
+
 test db = do
 	  putStrLn "before transaction:"
-	  printRecords db
-	  catchJust userErrors (transaction db (testTrans db)) (\e -> putStrLn $ "exception: " ++ e)
+	  handleErr $ printRecords db
+	  handleErr $ transaction db (testTrans db)
 	  putStrLn "after transaction:"
-	  printRecords db
+	  handleErr $ printRecords db
 
 
 main = argConnect test
