af6e40b8daf1b6888de41a4ed22331d5b56ba258
chucky <haskelldb-users@lists.sourceforge.net>
2004-03-22 03:13:48 -0800

    *** empty log message ***
    darcs-hash:20040322111348-29341-b48c3d488830f3136dfaab60381e834dd8a1a020.gz

--- src/Database/HaskellDB/DBSpec/DBSpecToDatabase.hs
+++ src/Database/HaskellDB/DBSpec/DBSpecToDatabase.hs
@@ -15,9 +15,27 @@ module Database.HaskellDB.DBSpec.DBSpecToDatabase
     (dbSpecToDatabase)
     where
 
+import Database.HaskellDB
+import Database.HaskellDB.FieldType
 import Database.HaskellDB.DBSpec
+import Data.List
 
--- FIXME: Write code for this
+-- | Prints a TInfo into an SQL statement
+tInfoToSql :: TInfo -> String
+tInfoToSql (TInfo {tname=n,cols=cs}) = "CREATE TABLE " ++ n ++ " ( " 
+			++ concat (intersperse "," 
+				   (map cInfoToSql cs))
+			++ ")"
 
-dbSpecToDatabase :: DBInfo -> IO ()
-dbSpecToDatabase dbinfo = undefined $ constructNonClashingDBInfo dbinfo
+-- | Prints a CInfo into an SQL statement
+cInfoToSql :: CInfo -> String
+cInfoToSql (CInfo {cname=n, descr=(ft,nullable)}) 
+    = n ++ " " ++ (sshow ft) ++	(if not nullable then " not null" else "")
+
+-- | Converts a DBInfo to a real life Database
+dbSpecToDatabase :: DBInfo -- ^ The DBInfo to generate from
+		 -> ((Database -> IO a) -> IO a) -- ^ A function that connects
+						 -- to a Database
+		 -> IO ()
+dbSpecToDatabase dbfunc dbinfo 
+    = return () --dbfunc (return ()) -- här borde stoppas in något som tar en Database som sista argument
