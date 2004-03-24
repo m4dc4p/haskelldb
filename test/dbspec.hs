e7e9f6592c86f2d605a5b7c29c7eb5a49db1cbf0
chucky <haskelldb-users@lists.sourceforge.net>
2004-03-24 02:19:31 -0800

    Used for testing generating from database
    darcs-hash:20040324101931-29341-db488fff3f810591b7addb582cdda21e7b5ebc95.gz

--- test/dbspec.hs
+++ test/dbspec.hs
@@ -3,6 +3,7 @@ module Main where
 import Database.HaskellDB
 import Database.HaskellDB.FieldType
 import Database.HaskellDB.DBSpec
+import Database.HaskellDB.DBSpec.DBSpecToDatabase
 
 import System.Environment (getArgs)
 
@@ -10,16 +11,16 @@ test = DBInfo {dbname = "ctest", opts = testopts, tbls = [testtbl1,testtbl2]}
 
 testopts = DBOptions {useBString = False}
 
-testtbl1 = TInfo {tname = "ctesttbl1", cols = [testcol11,testcol12]}
-testtbl2 = TInfo {tname = "ctesttbl2", cols = [testcol21,testcol22]}
+testtbl1 = TInfo {tname = "tokenfatgirls", cols = [testcol11,testcol12]}
+testtbl2 = TInfo {tname = "boysnthahood", cols = [testcol21,testcol22]}
 
-testcol11 = CInfo {cname = "ctestcol11", descr = (IntT,False)}
-testcol12 = CInfo {cname = "ctestcol12", descr = (BStrT 8,True)}
+testcol11 = CInfo {cname = "Karin", descr = (IntT,False)}
+testcol12 = CInfo {cname = "DinMamma", descr = (BStrT 8,True)}
 
-testcol21 = CInfo {cname = "ctestcol21", descr = (BStrT 6,False)}
-testcol22 = CInfo {cname = "ctestcol22", descr = (IntT,True)}
+testcol21 = CInfo {cname = "Karin", descr = (BStrT 6,False)}
+testcol22 = CInfo {cname = "DinMamma", descr = (IntT,True)}
 
 main = do 
        args <- getArgs
        let db = genericConnect (head args) (tail args)
-       db (\a -> dbSpecToDatabase a test)
+       db (dbSpecToDatabase test)
