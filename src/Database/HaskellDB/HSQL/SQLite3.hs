27a5362dea1b5ce123542e762ef330b4c24c2d7f
bjorn <bjorn@bringert.net>
2005-12-17 17:03:27 -0800

    Added hsql-sqlite3 backend.
    darcs-hash:20051218010327-6cdb2-4de289bc0be1e659ccac0b976303661935aeeefb.gz

---
+++ src/Database/HaskellDB/HSQL/SQLite3.hs
@@ -0,0 +1,58 @@
+-----------------------------------------------------------
+-- |
+-- Module      :  HaskellDB
+-- Copyright   :  Daan Leijen (c) 1999, daan@cs.uu.nl
+--                HWT Group (c) 2003, haskelldb-users@lists.sourceforge.net
+-- License     :  BSD-style
+-- 
+-- Maintainer  :  haskelldb-users@lists.sourceforge.net
+-- Stability   :  experimental
+-- Portability :  non-portable
+--
+-- Interface to the SQLite <http://www.hwaci.com/sw/sqlite/>
+-- databases.
+--
+-- $Revision: 1.6 $
+-----------------------------------------------------------
+module Database.HaskellDB.HSQL.SQLite3 (
+		   SQLiteOptions(..),
+		   sqliteConnect,
+		   driver
+		  ) where
+
+import Database.HaskellDB.Database
+import Database.HaskellDB.HSQL.Common
+import Database.HaskellDB.DriverAPI
+import qualified Database.HSQL.SQLite3 as SQLite3 (connect) 
+import System.IO
+
+data SQLiteOptions = SQLiteOptions { 
+				    filepath :: FilePath, -- ^ database file
+				    mode :: IOMode        -- ^ access mode
+                  		   }
+
+sqliteConnect :: MonadIO m => SQLiteOptions -> (Database -> m a) -> m a
+sqliteConnect = 
+    hsqlConnect (\opts -> SQLite3.connect 
+		            (filepath opts) (mode opts))
+
+sqliteConnectOpts :: [(String,String)] -> (Database -> IO a) -> IO a
+sqliteConnectOpts opts f = 
+    do
+    [a,b] <- getOptions ["filepath","mode"] opts
+    m <- readIOMode b
+    sqliteConnect (SQLiteOptions {filepath = a,
+				  mode = m}) f
+
+readIOMode :: Monad m => String -> m IOMode
+readIOMode s = 
+    case s of
+           "r" -> return ReadMode
+           "w" -> return WriteMode
+           "a" -> return AppendMode
+           "rw" -> return ReadWriteMode
+           _ -> case reads s of
+                             [(x,"")] -> return x
+                             _ -> fail $ "Bad IO mode: " ++ s
+
+driver = defaultdriver {connect = sqliteConnectOpts}
