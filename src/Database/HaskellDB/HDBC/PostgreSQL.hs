166846d2753514a151ad0ed7e91cb36d8d81b09f
bjorn <bjorn@bringert.net>
2005-12-27 10:05:03 -0800

    Added HDBC PostgreSQL driver.
    darcs-hash:20051227180503-6cdb2-5bccdd5e5b0fe7af6d93d7283c3bcf71378a78ce.gz

---
+++ src/Database/HaskellDB/HDBC/PostgreSQL.hs
@@ -0,0 +1,26 @@
+-----------------------------------------------------------
+-- |
+-- Module      :  Database.HaskellDB.HDBC.PostgreSQL
+-- Copyright   :  HWT Group (c) 2003, Bjorn Bringert (c) 2005
+-- License     :  BSD-style
+-- 
+-- Maintainer  :  haskelldb-users@lists.sourceforge.net
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-----------------------------------------------------------
+module Database.HaskellDB.HDBC.PostgreSQL (
+		      postgresqlConnect,
+		      driver
+		      ) where
+
+import Database.HaskellDB.Database
+import Database.HaskellDB.HDBC.Common
+import Database.HaskellDB.DriverAPI
+import Database.HDBC.PostgreSQL (connectPostgreSQL)
+
+postgresqlConnect :: MonadIO m => [(String,String)] -> (Database -> m a) -> m a
+postgresqlConnect opts = hdbcConnect connectPostgreSQL conninfo
+    where conninfo = unwords [ k ++ "=" ++ v | (k,v) <- opts]
+
+driver = defaultdriver {connect = postgresqlConnect}
