58c0acaf58d9fa4d1a588c088d534bc2141cc669
bjorn <bjorn@bringert.net>
2006-07-18 15:18:31 -0700

    Added test cases with strange values for most types.
    darcs-hash:20060718221831-6cdb2-fd036594570b845aa3b5bd899186ea2c9cfda78c.gz

--- test/TestCases.hs
+++ test/TestCases.hs
@@ -15,6 +15,8 @@ import Database.HaskellDB
 import Database.HaskellDB.HDBRec ((.=.))
 import Database.HaskellDB.Query (attributeName,tableName, constantRecord)
 
+import Control.Exception (catchDyn, throwDyn)
+import Data.Typeable
 import System.Time
 import Test.HUnit
 
@@ -26,7 +28,8 @@ allTests =
     dbtests [
              tableTests,
              fieldTests,
-             testDeleteEmpty
+             testDeleteEmpty,
+             testTransactionInsert
             ]
 
 tableTests = 
@@ -69,7 +72,34 @@ fieldTests =
              testField calendartime_tbl calendartime_data_1 TCalendartime.f01,
              testField calendartime_tbl calendartime_data_1 TCalendartime.f02,
              testField calendartime_tbl calendartime_data_1 TCalendartime.f03,
-             testField calendartime_tbl calendartime_data_1 TCalendartime.f04
+             testField calendartime_tbl calendartime_data_1 TCalendartime.f04,
+
+
+
+             testField string_tbl string_data_strange TString.f01,
+             testField string_tbl string_data_strange TString.f02,
+             testField string_tbl string_data_strange TString.f03,
+             testField string_tbl string_data_strange TString.f04,
+
+             testField int_tbl int_data_strange TInt.f01,
+             testField int_tbl int_data_strange TInt.f02,
+             testField int_tbl int_data_strange TInt.f03,
+             testField int_tbl int_data_strange TInt.f04,
+
+             testField integer_tbl integer_data_strange TInteger.f01,
+             testField integer_tbl integer_data_strange TInteger.f02,
+             testField integer_tbl integer_data_strange TInteger.f03,
+             testField integer_tbl integer_data_strange TInteger.f04,
+
+             testField double_tbl double_data_strange TDouble.f01,
+             testField double_tbl double_data_strange TDouble.f02,
+             testField double_tbl double_data_strange TDouble.f03,
+             testField double_tbl double_data_strange TDouble.f04,
+
+             testField calendartime_tbl calendartime_data_strange TCalendartime.f01,
+             testField calendartime_tbl calendartime_data_strange TCalendartime.f02,
+             testField calendartime_tbl calendartime_data_strange TCalendartime.f03,
+             testField calendartime_tbl calendartime_data_strange TCalendartime.f04
             ]
 
 testTable tbl r = 
@@ -86,26 +116,47 @@ testInsertAndQuery tbl r f = dbtest name $ \db ->
     do insert db tbl (constantRecord r)
        rs <- query db $ do t <- table tbl
                            project (f << t!f)
-       assertEqual "Bad result length" (length rs) 1
-       assertEqual "Bad field value" (head rs!f) (r!f)
+       assertEqual "Bad result length" 1 (length rs)
+       assertEqual "Bad field value" (r!f) (head rs!f) 
   where name = "insertAndQuery " ++ attributeName f
 
 testDistinct tbl r = dbtest name $ \db ->
     do insert db tbl (constantRecord r)
        insert db tbl (constantRecord r)
        rs <- query db $ table tbl
-       assertEqual "Bad result length" (length rs) 1
+       assertEqual "Bad result length" 1 (length rs)
   where name = "distinct " ++ tableName tbl
 
 testDeleteEmpty = dbtest "deleteEmpty" $ \db ->
-    do mapM_ (insert db hdb_t1) data1
+    do mapM_ (insert db hdb_t1) hdb_t1_data
        delete db hdb_t1 (\_ -> constant True)
        rs <- query db $ table hdb_t1
        assertBool "Query after complete delete is non-empty" (null rs)
 
 
+testTransactionInsert = dbtest "transactionInsert" $ \db ->
+     do let tr = do insert db hdb_t1 hdb_t1_data_1 
+                    throwDyn AbortTransaction
+        catchDyn (transaction db tr) handler
+        assertTableEmpty db hdb_t1
+       where 
+             handler :: AbortTransaction -> IO ()
+             handler _ = return ()
+
+data AbortTransaction = AbortTransaction
+                      deriving (Typeable)
+
+
+
+-- * Utilities
+
+assertTableEmpty db tbl =
+    do rs <- query db $ table tbl
+       assertBool "Table not empty" (null rs)
 
 -- * Test data
+
+string_data = [string_data_1,string_data_2,string_data_3]
 
 string_data_1 =
           TString.f01 .=. Just "foo" #
@@ -113,23 +164,59 @@ string_data_1 =
           TString.f03 .=. Nothing #
           TString.f04 .=. "baz"
 
+string_data_2 =
+          TString.f01 .=. Just "asdas" #
+          TString.f02 .=. "dast fsdf e" #
+          TString.f03 .=. Nothing #
+          TString.f04 .=. "jhasiude94"
+
+string_data_3 =
+          TString.f01 .=. Just "dafjht" #
+          TString.f02 .=. "adsfkasdjfklsadjfalsdf" #
+          TString.f03 .=. Nothing #
+          TString.f04 .=. "xxxxxxxx"
+
+string_data_strange = 
+          TString.f01 .=. Just "'\"\\;" #
+          TString.f02 .=. "\n\r\t " #
+          TString.f03 .=. Nothing #
+          TString.f04 .=. "\0\255\246"
+
 int_data_1 = 
           TInt.f01 .=. Just 42 #
           TInt.f02 .=. 43 #
           TInt.f03 .=. Nothing #
           TInt.f04 .=. (-1234)
 
+int_data_strange = 
+          TInt.f01 .=. Just 2147483647 #
+          TInt.f02 .=. (-2147483648) #
+          TInt.f03 .=. Nothing #
+          TInt.f04 .=. 0
+
 integer_data_1 = 
-          TInteger.f01 .=. Just 1234567890123456789012345678901234567890 #
+          TInteger.f01 .=. Just 1 #
           TInteger.f02 .=. 123 #
           TInteger.f03 .=. Nothing #
           TInteger.f04 .=. (-453453)
 
+integer_data_strange = 
+          TInteger.f01 .=. Just 1234567890123456789012345678901234567890 #
+          TInteger.f02 .=. (-35478572384578913475813465) #
+          TInteger.f03 .=. Nothing #
+          TInteger.f04 .=. (-1)
+
 double_data_1 = 
           TDouble.f01 .=. Just 0.0 #
-          TDouble.f02 .=. pi #
+          TDouble.f02 .=. 4.245 #
           TDouble.f03 .=. Nothing #
           TDouble.f04 .=. (-8.6e15)
+
+double_data_strange = 
+          TDouble.f01 .=. Just (-0.0) #
+          TDouble.f02 .=. pi #
+          TDouble.f03 .=. Nothing #
+          TDouble.f04 .=. (-8.6e37)
 
 bool_data_1 = 
           TBool.f01 .=. Just True #
@@ -143,7 +230,15 @@ calendartime_data_1 =
           TCalendartime.f03 .=. Nothing #
           TCalendartime.f04 .=. someTime
 
-data1 = [(
+calendartime_data_strange = 
+          TCalendartime.f01 .=. Just (epoch { ctYear = 1969 }) #
+          TCalendartime.f02 .=. someTime { ctYear = 2040 } #
+          TCalendartime.f03 .=. Nothing #
+          TCalendartime.f04 .=. epoch { ctYear = 1000 }
+
+hdb_t1_data = [hdb_t1_data_1]
+
+hdb_t1_data_1 = 
           t1f01 <<- Just "foo" #
           t1f02 <<- "bar" #
           t1f03 <<- Nothing #
@@ -174,12 +269,11 @@ data1 = [(
           t1f22 <<- epoch #
           t1f23 <<- Nothing #
           t1f24 <<- someTime
-         )]
 
 
 
 
-epoch :: CalendarTime
+
 epoch = CalendarTime {
                       ctYear = 1970,
                       ctMonth = January,
