011f007af3a8dd99e66d19551ba0b34bc3d9b827
jgbailey <jgbailey@gmail.com>
2009-04-09 16:56:42 -0700

    Added PrintQuery module
    Ignore-this: 62780df3fff1050841366a49bb26ccbd
    
    The Database.HaskellDB.PrintQuery module pretty prints the
    SqlSelect and other queries generated by HaskellDB. Useful
    for debugging.
    
    darcs-hash:20090409235642-447ee-97926903d6b8b5cc1117f8db685461f43cd598d3.gz

--- src/Database/HaskellDB.hs
+++ src/Database/HaskellDB.hs
@@ -5,7 +5,7 @@
 --                HWT Group (c) 2003, haskelldb-users@lists.sourceforge.net
 -- License     :  BSD-style
 -- 
--- Maintainer  :  haskelldb-users@lists.sourceforge.net
+-- Maintainer  :  "Justin Bailey" <jgbailey@gmail.com>
 -- Stability   :  experimental
 -- Portability :  non portable
 --
@@ -39,7 +39,7 @@
 --
 -----------------------------------------------------------
 module Database.HaskellDB
-	( Rel, Attr, Expr, Table, Query, OrderExpr
+	( Rel, Attr, Expr, ExprAggr, Table, Query, OrderExpr
 
         -- * Records
 	, HasField, Record, Select, ( # ), ( << ), (<<-), (!), (!.)
@@ -55,6 +55,8 @@ module Database.HaskellDB
 	, _not, like, _in, cat, _length
 	, isNull, notNull, fromNull
 	, constant, constJust
+	, param, namedParam, Args, func
+        , queryParams, Param
 	
 	, count, _sum, _max, _min, avg
 	, stddev, stddevP, variance, varianceP
@@ -78,6 +80,8 @@ module Database.HaskellDB
 import Database.HaskellDB.HDBRec
 -- PrimQuery type is imported so that haddock can find it.
 import Database.HaskellDB.PrimQuery (PrimQuery)
+import Database.HaskellDB.Sql (SqlSelect(SqlSelect, SqlBin), SqlExpr(..), SqlName)
+import qualified Database.HaskellDB.Sql as S (SqlSelect(..))
 import Database.HaskellDB.Sql.Generate (sqlQuery)
 import Database.HaskellDB.Sql.Default (defaultSqlGenerator)
 import Database.HaskellDB.Sql.Print (ppSql)
@@ -85,6 +89,11 @@ import Database.HaskellDB.Optimize  (optimize)
 import Database.HaskellDB.Query
 import Database.HaskellDB.Database
 import Text.PrettyPrint.HughesPJ (Doc)
+import Data.Foldable (foldr')
+
+-- | Represents a query parameter. Left parameters are indexed
+-- by position, while right parameters are named.
+type Param = Either Int String
 
 -- | Shows the optimized SQL for the query.
 instance Show (Query (Rel r)) where
@@ -104,4 +113,48 @@ showSql = show . ppSql . sqlQuery defaultSqlGenerator . optimize . runQuery
 
 -- | Shows the unoptimized SQL query.
 showSqlUnOpt :: Query (Rel r) -> String
-showSqlUnOpt = show . ppSql . sqlQuery defaultSqlGenerator . runQuery 
+showSqlUnOpt = show . ppSql . sqlQuery defaultSqlGenerator . runQuery
+
+-- | Get paramaters from a query in order.
+queryParams :: Query (Rel r) -> [Param]
+queryParams q = snd . indexParams . selectParams . toSelect $ q
+  where
+    -- Use foldr so we don't have to reverse parameter list built.
+    indexParams = foldr' renumber (1, [])
+    renumber (Just n) (idx, ps) = (idx, Right n : ps)
+    renumber Nothing (idx, ps) = (idx + 1, Left idx : ps)
+    toSelect = sqlQuery defaultSqlGenerator . optimize . runQuery 
+    -- | All parameters that are in the select, in the textual order
+    -- they will appear.
+    selectParams :: SqlSelect -> [Maybe SqlName]
+    selectParams select@(SqlSelect  { S.attrs = a, S.tables = t, S.criteria = c, S.groupby = g, S.orderby = o})
+        = (attrParams a ++) . (tableParams t ++) . (criteriaParams c ++) .
+            (groupByParams g ++) . orderByParams $ o
+      where
+        attrParams = getParams (exprParams . snd) 
+        tableParams = getParams (selectParams . snd) 
+        criteriaParams = getParams exprParams
+        groupByParams = getParams (exprParams . snd) 
+        orderByParams = getParams (exprParams . fst) 
+        getParams :: (a -> [Maybe SqlName]) -> [a] -> [Maybe SqlName]
+        getParams f = concatMap f
+        -- | All parameters in the expression, in the textual order
+        -- in which they will appear.
+        exprParams :: SqlExpr -> [Maybe SqlName]
+        exprParams (ColumnSqlExpr _) = [] 
+        exprParams (ConstSqlExpr _) = []
+        exprParams (ParamSqlExpr p _) = [p] 
+        exprParams (BinSqlExpr _ l r) = exprParams l ++ exprParams r
+        exprParams (PrefixSqlExpr _ e) = exprParams e
+        exprParams (PostfixSqlExpr _ e) = exprParams e
+        exprParams (FunSqlExpr _ es) = (concatMap exprParams es)
+        exprParams (CaseSqlExpr es e) =
+          let caseExprs = concatMap (\(l, r) -> exprParams l ++ exprParams r) es
+          in caseExprs ++ exprParams e
+        exprParams (ListSqlExpr es) = concatMap exprParams es
+        exprParams (ExistsSqlExpr select) = selectParams select
+        exprParams PlaceHolderSqlExpr = []
+        exprParams (ParensSqlExpr e) = exprParams e
+        exprParams (CastSqlExpr _ e) = exprParams e
+    selectParams (SqlBin _ l r) = selectParams l ++ selectParams r
+    selectParams _ = []
