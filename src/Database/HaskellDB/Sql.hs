2fd7b2febb6e75fd49ff28eb2af9a7e5173d1c68
jgbailey <jgbailey@gmail.com>
2010-08-25 14:18:00 -0700

    Sql representation improvements
    Ignore-this: f1bbd37fb6f9bc9db6136312bc976e45
    
    Added a AggrFunSqlExpr value to SqlExpr to represent aggregate functions
    vs. other functions. Added two fold functions to fold over SqlSelect
    and SqlExpr values.
    
    darcs-hash:20100825211800-447ee-679225ee29d5a6e19b57069b88a3cc67c6a15f64.gz

--- src/Database/HaskellDB/Sql.hs
+++ src/Database/HaskellDB/Sql.hs
@@ -29,7 +29,7 @@ module Database.HaskellDB.Sql (
                                SqlExpr(..),
                                Mark(..),
 
-                               newSelect
+                               newSelect, foldSqlExpr, foldSqlSelect
 	                      ) where
 
 
@@ -69,13 +69,31 @@ data SqlSelect  = SqlSelect {
                 | SqlTable SqlTable -- ^ Select a whole table.
                 | SqlEmpty -- ^ Empty select.
   deriving Show
-  
+
+-- | Transform a SqlSelect value.
+foldSqlSelect :: ([String] -> [(SqlColumn,SqlExpr)] 
+                           -> [(SqlTable, t)] 
+                           -> [SqlExpr] -> Maybe Mark 
+                           -> [(SqlExpr,SqlOrder)] 
+                           -> [String] -> t
+                 , String -> t -> t -> t
+                 , SqlTable -> t, t) 
+              -> SqlSelect 
+              -> t
+foldSqlSelect (select, bin, table, empty) = fold
+  where
+    fold (SqlSelect opt attr tab crit grou ord ext) = select opt attr (map (\(t, s) -> (t, fold s)) tab) crit grou ord ext
+    fold (SqlBin op left right) = bin op (fold left) (fold right)
+    fold (SqlTable tab) = table tab
+    fold SqlEmpty = empty
+
 -- | Expressions in SQL statements.
 data SqlExpr = ColumnSqlExpr  SqlColumn
              | BinSqlExpr     String SqlExpr SqlExpr
              | PrefixSqlExpr  String SqlExpr
              | PostfixSqlExpr String SqlExpr
              | FunSqlExpr     String [SqlExpr]
+             | AggrFunSqlExpr String [SqlExpr] -- ^ Aggregate functions separate from normal functions.
              | ConstSqlExpr   String
 	     | CaseSqlExpr    [(SqlExpr,SqlExpr)] SqlExpr
              | ListSqlExpr    [SqlExpr]
@@ -85,6 +103,41 @@ data SqlExpr = ColumnSqlExpr  SqlColumn
              | ParensSqlExpr SqlExpr
              | CastSqlExpr String SqlExpr 
   deriving Show
+
+-- | Transform a SqlExpr value.
+foldSqlExpr :: (SqlColumn -> t -- column
+               , String -> t -> t -> t -- bin
+               , String -> t -> t -- prefix
+               , String -> t -> t -- postfix
+               , String -> [t] -> t -- fun
+               , String -> [t] -> t -- aggr
+               , String -> t -- constant
+	       , [(t,t)] -> t -> t -- _case
+               , [t] -> t -- list
+               , SqlSelect -> t -- exists
+               , (Maybe SqlName) -> t -> t -- param
+               , t -- placeHolder
+               , t -> t -- parens
+               , String -> t -> t {- casts -}) 
+            -> SqlExpr 
+            -> t
+foldSqlExpr (column, bin, prefix, postfix, fun, aggr, constant, _case, list, exists, 
+                   param, placeHolder, parens, casts) = fold
+  where
+    fold (ColumnSqlExpr col) = column col
+    fold (BinSqlExpr op left right) = bin op (fold left) (fold right)
+    fold (PrefixSqlExpr op exp) = prefix op (fold exp)
+    fold (PostfixSqlExpr op exp) = postfix op (fold exp)
+    fold (FunSqlExpr name exprs) = fun name (map fold exprs)
+    fold (AggrFunSqlExpr name exprs) = aggr name (map fold exprs)
+    fold (ConstSqlExpr c) = constant c
+    fold (CaseSqlExpr cases def) = _case (map (\(e1, e2) -> (fold e1, fold e2)) cases) (fold def)
+    fold (ListSqlExpr exprs) = list (map fold exprs)
+    fold (ExistsSqlExpr select) = exists select
+    fold (ParamSqlExpr name exp) = param name (fold exp)
+    fold PlaceHolderSqlExpr = placeHolder
+    fold (ParensSqlExpr exp) = parens (fold exp)
+    fold (CastSqlExpr typ exp ) = casts typ (fold exp) 
 
 -- | Data type for SQL UPDATE statements.
 data SqlUpdate  = SqlUpdate SqlTable [(SqlColumn,SqlExpr)] [SqlExpr]
